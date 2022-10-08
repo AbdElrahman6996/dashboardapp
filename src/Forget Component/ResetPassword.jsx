fca2d5316f412bf2f94ecec2df31a3658cde0024
Abdelrahman Amir <abdoamir6996@gmail.com>
2022-10-08 20:23:06 +0200

    .

--- src/Forget Component/ResetPassword.jsx
+++ src/Forget Component/ResetPassword.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
-import './ResetPassword'
+import './ResetPassword.css'
+
 const ResetPassword = () => {
   return (
-    <div>ResetPassword</div>
+    <main className='resetpassword-component'>
+      <div className='ResetPassword-container'>
+        <h2>Reset Password</h2>
+        <p className='create-new'>Create your new password</p>
+        <div className='container'>
+          <div>
+            <p>Email</p>
+            <input type="text" placeholder='Email' />
+          </div>
+          <div>
+            <p>New Password</p>
+            <input type="text" placeholder='New Password' />
+          </div>
+          <div>
+            <p>Confirm New Password</p>
+            <input type="text" placeholder='Confirm New Password' />
+          </div>
+        </div>
+        <a href="/">Reset Password</a>
+      </div>
+    </main>
   )
 }
 
